c1ab338a79c638e4a4891017694a6b6b0ad3e81c
Bishoy Farouk Tawfik <bftawfik@gmail.com>
2019-02-11 11:13:51 +0200

    make it random

--- my-project/funcions/twitFunction.js
+++ my-project/funcions/twitFunction.js
@@ -1,6 +1,8 @@
 // require('dotenv').config();
 
 let twitterFunction = {
+  randomizerFactor: 13,
+  randomizerWorking: 1,
   searchForTweetsByHashtag: (Twit, hashtag='freeCodeCamp', count=1) => {
     hashtag = hashtag.indexOf('#') === 0 ? hashtag.slice(hashtag.indexOf('#')+1, hashtag.length) : hashtag;
     Twit.get('search/tweets', { 'q': '#'+hashtag, 'count': count }, function(err, data, response) {
@@ -35,16 +37,20 @@ let twitterFunction = {
   followHashtagsAndMyMention: (Twit, streamHashtags) => {
     let stream = Twit.stream('statuses/filter', {track: [...streamHashtags, process.env.SCREEN_NAME]});
     stream.on('tweet', function (tweet) {
+      let randomizer = Math.floor(Math.random() * twitterFunction.randomizerFactor) === twitterFunction.randomizerWorking;
+      // console.log(randomizer);
       if(!tweet.retweeted_status){
         let foundMe = [];
         if(tweet.entities.user_mentions.length > 0){
           foundMe = tweet.entities.user_mentions.filter(user => user.id_str === process.env.ID_STR);
         }
-        if(foundMe.length > 0){
-          twitterFunction.tweetMsg(Twit, 'Many thanks @' + tweet.user.screen_name + ' for mentioning me, but i\'m useless without my creator. You may want to contact him @' + process.env.MAKER_SCREEN_NAME+'!');
-        }else if(tweet.user.id_str !== process.env.ID_STR){
-          twitterFunction.retweetByTweetIdStr(Twit, tweet.id_str);
-          twitterFunction.followUser(Twit, tweet.user.id_str);
+        if(randomizer){
+          if(foundMe.length > 0){
+            twitterFunction.tweetMsg(Twit, 'Many thanks @' + tweet.user.screen_name + ' for mentioning me, but i\'m useless without my creator. You may want to contact him @' + process.env.MAKER_SCREEN_NAME+'!');
+          }else if(tweet.user.id_str !== process.env.ID_STR){
+            twitterFunction.retweetByTweetIdStr(Twit, tweet.id_str);
+            twitterFunction.followUser(Twit, tweet.user.id_str);
+          }
         }
       }
     });
